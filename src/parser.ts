677fe8dc231c8bcf6a626fd78866ed2979a7049c
ayecue <soerenwehmeier@googlemail.com>
2022-08-30 21:27:36 +0200

    0.4.2 bump core

--- src/parser.ts
+++ src/parser.ts
@@ -191,6 +191,7 @@ export default class Parser extends ParserBase {
     chunk.includes = me.includes;
     chunk.literals =  me.literals;
     chunk.scopes = me.scopes;
+    chunk.lines = me.astProvider.lines;
     chunk.end = new ASTPosition(me.token.line, me.token.lineRange[1]);
 
     return chunk;
