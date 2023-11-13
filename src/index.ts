85d6033d55db543dc9e3341b9b79f213531ec615
ayecue <soerenwehmeier@googlemail.com>
2023-11-13 23:23:02 +0100

    1.4.0 bump core

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { default as Parser, ParserOptions } from './parser';
 export * from './parser/ast';
 export * from './types/keywords';
 export * from './types/selector';
+export * from './types/operators';
+export * from './types/codes';
